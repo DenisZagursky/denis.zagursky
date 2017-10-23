86b17d225e8d51b9ed763c0d3389121dfae0b9cd
DenisZagursky <denis.Zagursky@gmail.com>
2017-10-23 20:57:47 +0300

    Created multi module maven application. Created modules: catalog,manager and inventory

---
+++ catalog/src/main/java/com/netcracker/zagursky/Application.java
@@ -0,0 +1,8 @@
+package com.netcracker.zagursky;
+
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ inventory/src/main/java/com/netcracker/zagursky/Application.java
@@ -0,0 +1,7 @@
+package com.netcracker.zagursky;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
